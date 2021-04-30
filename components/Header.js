f49b570769de2f6c251b46d80ed04523dfbaa399
Fredrik <fredrikf2000@gmail.com>
2021-05-01 00:34:33 +0200

    added more icons to header

--- components/Header.js
+++ components/Header.js
@@ -14,6 +14,11 @@ const Header = () => {
     <header className="">
       <div>
         <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image
         className="object-contain"
