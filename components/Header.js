0f8373a0bfa9513bf6f457a1f84b2b8b384d9f85
Fredrik <fredrikf2000@gmail.com>
2021-04-30 23:31:40 +0200

    added heroicons

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,20 @@
-import React from "react";
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className="">
+      <div>
+        <HeaderItem title="HOME" Icon={} />
+      </div>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      />
+    </header>
+  );
 };
 
 export default Header;
---
+++ components/HeaderItem.js
@@ -0,0 +1,5 @@
+const HeaderItem = ({}) => {
+  return <div></div>;
+};
+
+export default HeaderItem;
