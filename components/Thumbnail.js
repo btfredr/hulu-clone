4257917b9dc27269e1fbfca77c4439427f8cef8d
Fredrik <fredrikf2000@gmail.com>
2021-05-02 18:38:00 +0200

    .

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,7 +4,7 @@ import { ThumbUpIcon } from "@heroicons/react/outline";
 const Thumbnail = ({ result }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div>
+    <div className="group cursor-pointer">
       <Image
         layout="responsive"
         src={
@@ -17,8 +17,10 @@ const Thumbnail = ({ result }) => {
 
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
-        <p>
+        <h2 className="mt-1 text-2xl text-white transition-all-duration-100 ease-in-out group-hover:font-bold">
+          {result.title || result.original_name}
+        </h2>
+        <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type}`}{" "}
           {result.release_date || result.first_air_date}{" "}
           <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
